3a66d43c1f553e9bb4a73b29b4b86638cb58be85
mrsammydj <sammydjbaxter@outlook.com>
2023-03-29 19:01:37 -0400

    Rough GUI

---
+++ src/Board.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class Board extends JFrame {
+
+    private JLabel Player1JLabel;
+    private JLabel Player2JLabel;
+    private JLabel Player1PointValueJLabel;
+    private JLabel PointsJLabel;
+    private JLabel Player2PointsValueJLabel;
+    private JLabel TurnHeaderJLabel;
+    private JLabel PlayerTurnValueJLabel;
+    private JLabel LetterBagHeaderJLabel;
+    private JLabel LettersRemainingValueJLabel;
+    private JLabel PointJLabel;
+    private JButton CenterJButton;
+    private JLabel P1L1JLabel;
+    private JLabel P1L2JLabel;
+    private JLabel P1L3JLabel;
+    private JLabel P1L4JLabel;
+    private JLabel P1L5JLabel;
+    private JLabel P1L6JLabel;
+    private JLabel P1L7JLabel;
+    private JLabel P2L1JLabel;
+    private JLabel P2L2JLabel;
+    private JLabel P2L3JLabel;
+    private JLabel P2L4JLabel;
+    private JLabel P2L5JLabel;
+    private JLabel P2L6JLabel;
+    private JLabel P2L7JLabel;
+    private JLabel turnTitleJLabel;
+    private JLabel playerTurnJLabel;
+}
+
